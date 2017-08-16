bd61ca2550c2cb6a7a04eb0caa326c47adf453e2
Motoharu SOMEYA <maretol@outlook.com>
2017-08-16 11:49:56 +0900

    Add files via upload
    Powershell setting file

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,31 @@
+Set-Location "C:\Users\Motoharu SOMEYA\Documents"
+Set-Alias open "Invoke-Item"
+Set-Alias less more
+Set-Alias utf8 Change-Encoding-utf
+Set-Alias sjis Change-Encoding-shiftjis
+Set-Alias sudo Elevate-Process
+
+function Change-Encoding-utf
+{
+    chcp 65001;
+}
+
+function Change-Encoding-shiftjis
+{
+    chcp 932;
+}
+
+function Elevate-Process
+{
+    $file, [string]$arguments = $args;
+    $psi = New-Object System.Diagnostics.ProcessStartInfo $file;
+    $psi.Arguments = $arguments;
+    $psi.Verb = "runas";
+    $psi.WorkingDirectory = Get-Location;
+    [System.Diagnostics.Process]::Start($psi);
+}
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
